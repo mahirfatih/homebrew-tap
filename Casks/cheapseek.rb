aac27c2fabcae61052dda38e1bd31792b1e02dca
Mahir F. UNAL <irtibat@mahirfatih.com>
2026-09-22 20:35:49 +0300

    cheapseek 1.0.0

--- Casks/cheapseek.rb
+++ Casks/cheapseek.rb
@@ -1,6 +1,6 @@
 cask "cheapseek" do
   version "1.0.0"
-  sha256 "f38405407b4ae666f6b8e4ff513a110b404d9ba44bd2bf0946b5eb5d6b61109c"
+  sha256 "c072343b76c1832add1797da3d493dfb85e8b23338f1fa66bf80aed13e7a6365"
 
   url "https://github.com/mahirfatih/cheapseek/releases/download/v#{version}/CheapSeek-#{version}.dmg"
   name "CheapSeek"
@@ -10,6 +10,12 @@ cask "cheapseek" do
   depends_on macos: ">= :sonoma"
 
   app "CheapSeek.app"
+
+  caveats <<~EOS
+    CheapSeek is not notarized yet. On first launch, right-click the app and
+    choose Open, or run:
+      xattr -dr com.apple.quarantine /Applications/CheapSeek.app
+  EOS
 
   zap trash: [
     "~/Library/Preferences/com.labrus.CheapSeek.plist",
